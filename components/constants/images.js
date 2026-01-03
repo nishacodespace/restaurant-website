027552336220e7fe661cd2ce53c4936045c5eef1
Nisha Mishra <thisisnisha0809@gmail.com>
2026-01-04 00:24:03 +0530

    Add files via upload

---
+++ components/constants/images.js
@@ -0,0 +1,32 @@
+import i1 from "/assets/i1.jpg";
+import i2 from "/assets/i2.jpg";
+import i3 from "/assets/i3.jpg";
+import i4 from "/assets/i4.jpg";
+import i5 from "/assets/i5.jpg";
+import interior2 from "/assets/interior2.jpg";
+import interior3 from "/assets/interior3.jpg";
+import interior1 from "/assets/interior1.jpg";
+import homePicture from "/assets/homepicture.jpg";
+import spoon from "/assets/spoon.png";
+import navbarLogo from "/assets/navbar-logo.jpg";
+import chef from "/assets/chef.jpg";
+
+
+const galleryImages={
+    i1,
+    i2,
+    i3,
+    i4,
+    i5
+};
+
+const aboutImages={
+    interior1,
+    interior2,
+    interior3
+};
+const homeImages={
+    homePicture,
+    navbarLogo
+};
+export { galleryImages, aboutImages, homeImages,spoon,chef };
