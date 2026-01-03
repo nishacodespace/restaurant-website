a06bdba58ff371eed507196fdf3d620b7e2a1aab
Nisha Mishra <thisisnisha0809@gmail.com>
2026-01-03 23:48:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react'
-export default defineConfig({plugins: [react()]})
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            assets: path.resolve(__dirname, 'src/assets'),
+            components: path.resolve(__dirname, 'src/components')
+        }
+    }
+});
